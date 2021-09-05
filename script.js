0b77e1bad50a06dccabf143d877edba80d889f9a
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-05 18:34:31 -0400

    add functions

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 // The Document Object Model (DOM) is a programming interface for web documents. It represents the page so // that programs can change the document structure, style, and content. The DOM represents the document as // nodes and objects; that way, programming languages can interact with the page.
 
+
+class Calculator {
+    constructor(prevOperandTextElement, currentOperandTextElement) {
+        this.prevOperandTextElement = prevOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    // function
+    clear() {
+
+    }
+
+    // function
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        
+    }
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals')
