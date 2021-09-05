642c610a5e4355c6cd5aa379be78c37e7fb7e30e
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-05 18:30:17 -0400

    add html dom

---
+++ script.js
@@ -0,0 +1,13 @@
+// The Document Object Model (DOM) is a programming interface for web documents. It represents the page so // that programs can change the document structure, style, and content. The DOM represents the document as // nodes and objects; that way, programming languages can interact with the page.
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals')
+const deleteButton = document.querySelector('[data-delete')
+const allClearButton = document.querySelector('[data-all]')
+const prevOperandTextElement = document.querySelector('[data-prev-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+
+
