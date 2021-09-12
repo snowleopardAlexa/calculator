d443ce33b84295895f75defcb8ba0a2427b35d82
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-11 21:31:58 -0400

    add allclearbutton

--- script.js
+++ script.js
@@ -120,3 +120,8 @@ equalsButton.addEventListener('click', button => {
     calculator.updateDisplay()
 })
 
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
