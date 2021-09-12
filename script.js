1999e14364c66498ee26ef8270b70e2b9dca7c0b
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-11 21:16:09 -0400

    add compute function

--- script.js
+++ script.js
@@ -36,5 +36,31 @@ class Calculator {
         this.currentOperand = ''
     }
 
+    compute() {
+        let computation 
+        const prev = parseFloat(this.prevOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) ||  isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current 
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default: 
+            return            
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.prevOperand = ''
+    }
+
     
 }
